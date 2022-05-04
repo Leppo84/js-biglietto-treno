5f1b8d4d2a2baee1515790da91ed8c9f2ce49093
LeppoXena <francesco.lepore@xenagames.com>
2022-05-04 17:20:52 +0200

    Content fixed, costs rounded up

--- js/myscript.js
+++ js/myscript.js
@@ -1,29 +1,37 @@
 const guestage = parseInt(prompt("Inserisci la tua età"));
 
+console.log(guestage);
+
 const guestkm = parseInt(prompt("Inserisci i kilometri che vuoi viaggiare"));
 
-const guestprice = guestkm * 0.21;
+let guestprice = guestkm * 0.21;
 
 console.log(guestprice);
 
-const discunder = guestprice * 0.80;
+let discunder = guestprice * 0.80;
 
-const discover = guestprice * 0.60;
+let discover = guestprice * 0.60;
 
 document.getElementById("kmoutput").innerHTML=guestkm + " €";
+
+guestprice=guestprice.toFixed(2);
 
 document.getElementById("costoutput").innerHTML=guestprice + " €";
 
 if (guestage<=18) {
+    discunder=discunder.toFixed(2);
+
     document.getElementById("finalcostoutput").innerHTML=discunder + " €";
-    document.getElementById("ageoutput").innerHTML="ancora minorenne faremo uno sconto del 20%";
+    document.getElementById("ageoutput").innerHTML="tu giovane, mossi a pietà, faremo uno sconticino del 20%. Tanto paga pantaloni.";
 }
 else if (guestage>=60) {
-    document.getElementById("finalcostoutput").innerHTML=discover + " €";
-    document.getElementById("ageoutput").innerHTML="poveri vecchierelli faremo uno sconto del 40%";
+    discover=discover.toFixed(2);
+
+    document.getElementById("finalcostoutput").innerHTML=discover + " €.";
+    document.getElementById("ageoutput").innerHTML="Lei un po' avanti con l'età applicheremo ben il 40% di sconto!";
 }
 else {
-    document.getElementById("finalcostoutput").innerHTML="paga";
-    document.getElementById("ageoutput").innerHTML="nè carne nè pesce, per voi niente sconti.";
+    document.getElementById("finalcostoutput").innerHTML="è uguale a prima. Paga.";
+    document.getElementById("ageoutput").innerHTML="nè carne nè pesce, per te nessuna pietà. Niente sconti.";
 }
 
